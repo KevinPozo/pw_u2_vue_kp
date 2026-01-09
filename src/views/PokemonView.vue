6629183ae74dd5c86d19ff05ff89127083fd79b8
Kevin Fernando Pozo Maldonado <kevinferpozo@gmail.com>
2026-01-08 19:43:31 -0500

    Taller 21

--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,9 +1,23 @@
 <template>
-    <PokemonImagen :pokemonId="pokemonGanador" />
-    <PokemonOpciones :listaPokemon="pokemonaArr" :idCorrecto="pokemonGanador" :idSeleccionado="seleccionUsuario"
-        @seleccionado="evaluarGanador($event)" />
-    <div v-if="mensaje" class="mensaje-resultado">
-        <h1>{{ mensaje }}</h1>
+    <div>
+        <PokemonImagen
+            v-if="mostrarImagen && pokemonGanador"
+            :pokemonId="pokemonGanador"
+        />
+
+        <PokemonOpciones
+            :listaPokemon="pokemonaArr"
+            :idCorrecto="pokemonGanador"
+            :idSeleccionado="seleccionUsuario"
+            @seleccionado="evaluarGanador($event)"
+        />
+
+        <div v-if="mensaje" class="mensaje-resultado">
+            <h1>{{ mensaje }}</h1>
+        </div>
+
+        <button @click="destruirImagen">Destruir Imagen</button>
+        <button @click="mostrarImagenMetodo">Mostrar Imagen</button>
     </div>
 </template>
 
@@ -11,6 +25,7 @@
 import PokemonOpciones from '@/components/PokemonOpciones.vue';
 import PokemonImagen from '@/components/PokemonImagen.vue';
 import { obtenerVectorNumericoFacade, obtenerAleatorioFacade } from '../clients/PokemonClient';
+
 export default {
     components: {
         PokemonOpciones,
@@ -18,14 +33,37 @@ export default {
     },
     data() {
         return {
+            mostrarImagen: true,
             pokemonaArr: [],
             pokemonGanador: null,
             mensaje: null,
             seleccionUsuario: null
         }
     },
+    beforeCreate() {
+        console.log("beforeCreate: instancia creada");
+    },
+    created() {
+        console.log("created: data y métodos inicializados");
+    },
+    beforeMount() {
+        console.log("beforeMount: antes del primer render");
+    },
     mounted() {
+        console.log("mounted: componente montado");
         this.iniciarJuego();
+    },
+    beforeUpdate() {
+        console.log("beforeUpdate: antes de re-render");
+    },
+    updated() {
+        console.log("updated: después del re-render");
+    },
+    beforeUnmount() {
+        console.log("beforeUnmount: justo antes de destruir el componente");
+    },
+    unmounted() {
+        console.log("unmounted: componente destruido");
     },
     methods: {
         async iniciarJuego() {
@@ -34,20 +72,29 @@ export default {
             this.pokemonGanador = this.pokemonaArr[idAleatorio].id;
             this.mensaje = null;
             this.seleccionUsuario = null;
+            this.mostrarImagen = true;
         },
         evaluarGanador(idGanador) {
             if (this.seleccionUsuario) return;
+
             this.seleccionUsuario = idGanador;
-            if (idGanador == this.pokemonGanador) {
+
+            if (idGanador === this.pokemonGanador) {
                 this.mensaje = "Pokemon Correcto";
             } else {
                 this.mensaje = "Pokemon Incorrecto";
             }
+
             setTimeout(() => {
                 this.iniciarJuego();
             }, 2000);
+        },
+        destruirImagen() {
+            this.mostrarImagen = false;
+        },
+        mostrarImagenMetodo() {
+            this.mostrarImagen = true;
         }
-
     }
 }
 </script>
@@ -68,6 +115,4 @@ export default {
     color: #2c3e50;
     margin: 0;
 }
-
-
 </style>
